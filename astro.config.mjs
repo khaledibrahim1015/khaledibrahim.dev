9cca4c666d668a4f29de6a4151f6af0cff15e7f7
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2026-02-20 07:04:52 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://khaledibrahim1015.github.io',
-  // base: '/khaledibrahim.dev', // This is required because your repo is a project page
+  base: '/khaledibrahim.dev', // This is required because your repo is a project page
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
